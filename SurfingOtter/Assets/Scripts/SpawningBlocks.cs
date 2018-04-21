1c416b9246687010cfe75bf118a325dff654d5b9
Eric Tinoco <tino5168@csumb.ad.csumb.edu>
2018-04-21 00:04:32 -0700

    pulled, and pushed, spawning boxes

--- SurfingOtter/Assets/Scripts/SpawningBlocks.cs
+++ SurfingOtter/Assets/Scripts/SpawningBlocks.cs
@@ -7,63 +7,43 @@ public class SpawningBlocks : MonoBehaviour {
     public Transform[] spawn_locations; // To hold the different spawn locations
     public GameObject[] boxes;          // To hold the prefabs of the boxes
 
-    private List<int> current_locations = new List<int>(); // To hold a list of integers which will determine the location
+    private GameObject player;          // To hold the game object of the player, later to get the transform
     private GameObject boxInstance;     // To hold the instance of the first box
     private Vector3 box_placement;      // To hold the placement location of the bear
     private int current_spawn_location; // To hold a random number for the spawn location within length of the array spawn_locations
-    private int spawn_box;              // To hold an the counter until next box spawns.
+    private int randomBox;              // To hold an integer for the random box
+    private int spawn_box_timer;        // To hold an the counter until next box spawns.
 
     // Use this for initialization
     void Start()
     {
-        current_locations.Add(100); // just adding a integer to start, when using conatains method
-        spawn_box = 180;
+        spawn_box_timer = 180;  // set timer to 3 seconds
+        player = GameObject.FindGameObjectWithTag("Player");    // set player to the tag of "Player"
     }
 
     // Update is called once per frame
     void Update()
     {
-        spawn_box--;
-        if(spawn_box <= 0)
+        transform.position = new Vector2(player.transform.position.x, transform.position.y);    // Follow the player by it's X axis
+
+        spawn_box_timer--;    // Spawn box cyle start
+
+        // once spawn_box_timer reaches 0, then spawn a new box.
+        if(spawn_box_timer <= 0)
         {
-            SpawnBox();
-            spawn_box = 180;
+            SpawnBox();         // spawn box
+            spawn_box_timer = 180;    // return timer of the spawn_box_timer
         }
     }
-
-    /// <summary>
-    /// This function get's an index that isnt being used.
-    /// It stores an integer into a List<int> current_locations.
-    /// It makes sure to return a value that isnt within the List.
-    /// </summary>
-    /// <returns>
-    /// An integer, from 0 - waypoints.length (0-14)
-    /// </returns>
-    private int GetIndex()
-    {
-        int index = Random.Range(0, spawn_locations.Length);  // Declare and initialize index to be a random number from 0-14 (waypoint.length)
-
-        // while loop: Validation method that if the integer "index" is already in the List "current_locations" then,
-        // try for a new random integer, until one is not located in that list.
-        while (current_locations.Contains(index))
-        {
-            index = Random.Range(0, spawn_locations.Length);
-        }
-
-        current_locations.Add(index);   // Add integer to List
-
-        return index;                   // Return integer
-    }
-
     
     /// <summary>
     /// This function will spawn a box
     /// </summary>
     void SpawnBox()
     {
-        int randomBox = Random.Range(0, boxes.Length);                            // Get a random int for the box
-        current_spawn_location = GetIndex();
-        box_placement = spawn_locations[current_spawn_location].position;
-        boxInstance = Instantiate(boxes[randomBox], box_placement, Quaternion.identity);
+        randomBox = Random.Range(0, boxes.Length);                                          // Get a random int for the box
+        current_spawn_location = Random.Range(0, boxes.Length);                             // Get a random int for the spawn location
+        box_placement = spawn_locations[current_spawn_location].position;                   // initialize the spawn location position
+        boxInstance = Instantiate(boxes[randomBox], box_placement, Quaternion.identity);    // Instantiate box, and save in boxInstance.
     }
 }
