e7c2fdc6734e0cca6b3681cf1fbc8733dfff1d18
Matthew Connolly <conn6273@csumb.ad.csumb.edu>
2018-04-21 00:38:08 -0700

    gitignore stuff

---
+++ SurfingOtter/Assets/Scripts/SceneManage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour {
+
+	// Use this for initialization
+    public void LoadLevel(string valueName)
+    {
+        SceneManager.LoadScene(valueName);
+    }
+	void Start () {
+		
+	}
+
+    //public void Quit()
+    //{
+        
+    //}
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
