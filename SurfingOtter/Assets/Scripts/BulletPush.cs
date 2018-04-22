7836095450140dfbb03ee2b9aad8a5f720ceb277
Julia Werner <wern4701@csumb.ad.csumb.edu>
2018-04-21 20:15:21 -0700

    Added pushing and destroying of bullet

--- SurfingOtter/Assets/Scripts/BulletPush.cs
+++ SurfingOtter/Assets/Scripts/BulletPush.cs
@@ -16,36 +16,13 @@ public class BulletPush : MonoBehaviour {
     {
         if(collision.gameObject.tag == "Block")
         {
-            if (shootingMode == 1)
-            {
-                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.forward * 1000);
-                //foreach(ContactPoint contact in collision.contacts)
-                //{
-                //    if(contact.thisCollider==areaLeft)
-                //    {
-                //        collision.rigidbody.AddForce(collision.transform.right * 100);
-                //    }
-                //}
-                //Vector2 direction = transform.InverseTransformPoint(collision.transform.position); //this helps us find which direction the object collided from
-
-                //if (direction.x > 0f)
-                //{
-                //    Debug.Log("The object collided with the right side of the ball!");
-                //}
-                //else if (direction.x < 0f)
-                //{
-                //    Debug.Log("The object collided with the left side of the ball!");
-                //}
-
-                //Destroy(gameObject);
-            }
             if(shootingMode==2)
             {
                 Debug.Log("Rotating");
                 collision.transform.Rotate(0, 0, 90);
                 Destroy(gameObject);
             }
-            if(shootingMode==3)
+            if (shootingMode == 3)
             {
                 Destroy(collision.gameObject);
                 Destroy(gameObject);
