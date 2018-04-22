cca3a2ed43e261091a42c90f0210d327e9ce9ce3
Julia Werner <wern4701@csumb.ad.csumb.edu>
2018-04-22 01:43:23 -0700

    Words on item collecting

--- SurfingOtter/Assets/Scripts/WordItem.cs
+++ SurfingOtter/Assets/Scripts/WordItem.cs
@@ -1,7 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WordItem : MonoBehaviour {
     public string word;
+    public Text display;
+
+    private void Awake()
+    {
+        display.text = word;
+    }
 }
